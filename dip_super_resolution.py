096bffe57bf8db77b92f378f3aa7319ebc78927d
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-11-27 10:39:05 +0000

    script version

---
+++ dip_super_resolution.py
@@ -0,0 +1,263 @@
+import numpy as np
+from models import *
+import torch
+from skimage.metrics import peak_signal_noise_ratio
+from utils.denoising_utils import *
+
+torch.backends.cudnn.enabled = True
+torch.backends.cudnn.benchmark =True
+
+use_gpu = True
+
+if use_gpu:
+    dtype = torch.cuda.FloatTensor
+else:
+    dtype = torch.float32
+
+imsize =-1
+PLOT = True
+
+# # Load the high resolution image and low resolution image
+
+from dataset import Div2kDataset, Mode
+from pathlib import Path
+
+from utils.common_utils import pil_to_np, np_to_pil, np_to_torch, torch_to_np
+from utils.sr_utils import crop_image
+
+#low_res_path = Path("dataset/DIV2K_train_LR_x8")
+#high_res_path = Path("dataset/DIV2K_train_HR")
+#dataset = Div2kDataset(low_res_path, high_res_path, Mode.TRAIN)
+
+high = Image.open("0001.png")
+low = Image.open("0001x8.png")
+
+low = crop_image(low)
+high = crop_image(high)
+
+low_np = pil_to_np(low)
+high_np = pil_to_np(high)
+
+# # Set up the parameters for training
+# 
+# Main parameters
+# 
+# 1. **num_iter** controls the number of iterations for training
+# 
+# 2. **LR** controls the learning rate
+# 
+# 3. Two types of **optimizer** exists here. (1) **adam** (2)
+#  **LBFGS**
+# 
+# In function **get_net**, there exists five types of Neural Network (1) **ResNet** (2) **skip** (3) **texture_nets** (4) **UNet** (5) **identity**
+# 
+# 
+# Variable **mse** controls the type of loss here.
+
+pad = 'reflection'
+OPT_OVER = 'net' # 'net,input'
+
+#training parameters
+reg_noise_std = 1./30.
+LR = 0.01
+
+OPTIMIZER='adam' # 'LBFGS'
+show_every = 100
+exp_weight=0.99
+
+num_iter = 500
+input_depth = 3
+figsize = 4
+
+INPUT = 'noise' # 'meshgrid'
+#Network structure
+net = get_net(input_depth, 'skip', pad,
+              skip_n33d=128,
+              skip_n33u=128,
+              skip_n11=4,
+              num_scales=5,
+              upsample_mode='bilinear').type(dtype)
+
+
+net_input = get_noise(input_depth, INPUT, (high.size[1], high.size[0])).type(dtype).detach()
+
+# Compute number of parameters
+s  = sum([np.prod(list(p.size())) for p in net.parameters()])
+print ('Number of params: %d' % s)
+
+# Loss
+mse = torch.nn.MSELoss().type(dtype)
+
+low_torch = np_to_torch(low_np).type(dtype)
+high_torch = np_to_torch(high_np).type(dtype)
+
+# **Architecture of the Network of skip**
+
+import torch
+import torch.nn as nn
+from models.common import *
+
+def skip(
+        num_input_channels=2, num_output_channels=3,
+        num_channels_down=[16, 32, 64, 128, 128], num_channels_up=[16, 32, 64, 128, 128], num_channels_skip=[4, 4, 4, 4, 4],
+        filter_size_down=3, filter_size_up=3, filter_skip_size=1,
+        need_sigmoid=True, need_bias=True,
+        pad='zero', upsample_mode='nearest', downsample_mode='stride', act_fun='LeakyReLU',
+        need1x1_up=True):
+    """Assembles encoder-decoder with skip connections.
+
+    Arguments:
+        act_fun: Either string 'LeakyReLU|Swish|ELU|none' or module (e.g. nn.ReLU)
+        pad (string): zero|reflection (default: 'zero')
+        upsample_mode (string): 'nearest|bilinear' (default: 'nearest')
+        downsample_mode (string): 'stride|avg|max|lanczos2' (default: 'stride')
+
+    """
+    assert len(num_channels_down) == len(num_channels_up) == len(num_channels_skip)
+
+    n_scales = len(num_channels_down)
+
+    if not (isinstance(upsample_mode, list) or isinstance(upsample_mode, tuple)) :
+        upsample_mode   = [upsample_mode]*n_scales
+
+    if not (isinstance(downsample_mode, list)or isinstance(downsample_mode, tuple)):
+        downsample_mode   = [downsample_mode]*n_scales
+
+    if not (isinstance(filter_size_down, list) or isinstance(filter_size_down, tuple)) :
+        filter_size_down   = [filter_size_down]*n_scales
+
+    if not (isinstance(filter_size_up, list) or isinstance(filter_size_up, tuple)) :
+        filter_size_up   = [filter_size_up]*n_scales
+
+    last_scale = n_scales - 1
+
+    cur_depth = None
+
+    model = nn.Sequential()
+    model_tmp = model
+
+    input_depth = num_input_channels
+    for i in range(len(num_channels_down)):
+
+        deeper = nn.Sequential()
+        skip = nn.Sequential()
+
+        if num_channels_skip[i] != 0:
+            model_tmp.add(Concat(1, skip, deeper))
+        else:
+            model_tmp.add(deeper)
+
+        model_tmp.add(bn(num_channels_skip[i] + (num_channels_up[i + 1] if i < last_scale else num_channels_down[i])))
+
+        if num_channels_skip[i] != 0:
+            skip.add(conv(input_depth, num_channels_skip[i], filter_skip_size, bias=need_bias, pad=pad))
+            skip.add(bn(num_channels_skip[i]))
+            skip.add(act(act_fun))
+
+        # skip.add(Concat(2, GenNoise(nums_noise[i]), skip_part))
+
+        deeper.add(conv(input_depth, num_channels_down[i], filter_size_down[i], 2, bias=need_bias, pad=pad, downsample_mode=downsample_mode[i]))
+        deeper.add(bn(num_channels_down[i]))
+        deeper.add(act(act_fun))
+
+        deeper.add(conv(num_channels_down[i], num_channels_down[i], filter_size_down[i], bias=need_bias, pad=pad))
+        deeper.add(bn(num_channels_down[i]))
+        deeper.add(act(act_fun))
+
+        deeper_main = nn.Sequential()
+
+        if i == len(num_channels_down) - 1:
+            # The deepest
+            k = num_channels_down[i]
+        else:
+            deeper.add(deeper_main)
+            k = num_channels_up[i + 1]
+
+        deeper.add(nn.Upsample(scale_factor=2, mode=upsample_mode[i]))
+
+        model_tmp.add(conv(num_channels_skip[i] + k, num_channels_up[i], filter_size_up[i], 1, bias=need_bias, pad=pad))
+        model_tmp.add(bn(num_channels_up[i]))
+        model_tmp.add(act(act_fun))
+
+        if need1x1_up:
+            model_tmp.add(conv(num_channels_up[i], num_channels_up[i], 1, bias=need_bias, pad=pad))
+            model_tmp.add(bn(num_channels_up[i]))
+            model_tmp.add(act(act_fun))
+
+        input_depth = num_channels_down[i]
+        model_tmp = deeper_main
+
+    model.add(conv(num_channels_up[0], num_output_channels, 1, bias=need_bias, pad=pad))
+    if need_sigmoid:
+        model.add(nn.Sigmoid())
+
+    return model
+
+net_input_saved = net_input.detach().clone()
+
+noise = net_input.detach().clone()
+#smoothing image
+out_avg = net_input_saved
+
+#without smoothing
+#out_avg = None
+
+last_net = None
+psrn_noisy_last = 0
+
+i = 0
+def closure():
+    global i, out_avg, psrn_noisy_last, last_net, net_input
+
+    if reg_noise_std > 0:
+        net_input = net_input_saved + (noise.normal_() * reg_noise_std)
+
+    out = net(net_input)
+
+    # Smoothing
+    if out_avg is None:
+        out_avg = out.detach()
+    else:
+        out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
+
+    #total_loss = mse(out, img_noisy_torch)
+    total_loss = mse(out, high_torch)
+    total_loss.backward()
+
+    #evaluation with psrn
+    psrn_noisy = peak_signal_noise_ratio(low_np, out.detach().cpu().numpy()[0])
+    psrn_gt    = peak_signal_noise_ratio(high_np, out.detach().cpu().numpy()[0])
+    psrn_gt_sm = peak_signal_noise_ratio(high_np, out_avg.detach().cpu().numpy()[0])
+
+    if  PLOT and i % 10 == 0:
+         print ('Iteration: ', i, ' Loss: ', total_loss.item(), ' PSRN_gt: ', psrn_gt, ' PSNR_gt_sm: ', psrn_gt_sm)
+    #print ('Iteration %05d    Loss %f   PSRN_gt: %f PSNR_gt_sm: %f' % (i, total_loss.item(), psrn_gt, psrn_gt_sm), '\r', end='')
+    if  PLOT and i % show_every == 0:
+        #out_np = torch_to_np(out)
+        plot_image_grid([np.clip(low_np, 0, 1),
+                         np.clip(torch_to_np(out_avg), 0, 1)], factor=figsize, nrow=2)
+
+    # Backtracking
+    if i % show_every:
+        if psrn_noisy - psrn_noisy_last < -5:
+            print('Falling back to previous checkpoint.')
+
+            for new_param, net_param in zip(last_net, net.parameters()):
+                net_param.data.copy_(new_param.cuda())
+
+            return total_loss*0
+        else:
+            last_net = [x.detach().cpu() for x in net.parameters()]
+            psrn_noisy_last = psrn_noisy
+
+    i += 1
+
+    return total_loss
+
+p = get_params(OPT_OVER, net, net_input)
+optimize(OPTIMIZER, p, closure, LR, num_iter)
+
+# # Show the result
+
+out_np = torch_to_np(net(net_input))
+q = plot_image_grid([np.clip(out_np, 0, 1), high_np], factor=13);
